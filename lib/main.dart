4f6514cd7ca76e7e5f6f00b29eeaf0b4fe4b945a
YousefBawaliz <yousef2k012@gmail.com>
2022-11-11 14:31:55 +0300

    alright

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.red,
+        cardColor: Colors.yellow),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
